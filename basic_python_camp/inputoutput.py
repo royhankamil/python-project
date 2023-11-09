c64c0d22400791baac8186772a26d310f5fbe009
Achmad Royhan Kamil <royhan1125@gmail.com>
2023-11-09 22:57:27 +0700

    menambahkan-basic-output

---
+++ basic_python_camp/inputoutput.py
@@ -0,0 +1,12 @@
+
+
+"""
+Ubah "Hello World!" menjadi pesan yang ingin Anda sampaikan pada dunia.
+"""
+
+print("Hello World!")
+
+nama = input("masukkan nama: ")
+cuaca = "mendung"
+
+print("Hello", nama, "selamat siang, hari ini cuacanya", cuaca)
