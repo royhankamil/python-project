57b3dad3ef1ffe96a6dea006c71001220a69a0e8
Achmad Royhan Kamil <114550416+royhankamil@users.noreply.github.com>
2023-11-10 13:23:49 +0700

    comment_input

--- basic_python_camp/inputoutput.py
+++ basic_python_camp/inputoutput.py
@@ -4,9 +4,37 @@
 Ubah "Hello World!" menjadi pesan yang ingin Anda sampaikan pada dunia.
 """
 
+# ini adalah comment yang tidak akan dieksekusi mesin
+"""
+ini adalah block comment yang akan diabaikan mesin
+dan teks ini juga
+"""
+
 print("Hello World!")
 
-nama = input("masukkan nama: ")
+nama = input("masukkan nama: ") # input
 cuaca = "mendung"
 
-print("Hello", nama, "selamat siang, hari ini cuacanya", cuaca)
+print("Hello", nama, "selamat siang, hari ini cuacanya", cuaca) # output
+
+
+greeting = 'Hey there im using whatsapp'   #  Ini adalah assignment
+print(greeting, "yo hell nah", sep=", ")
+
+"""
+Output: Hey there im using whatsapp, yo hell nah
+"""
+
+
+"""
+addition adalah variabel yang bernilai ekspresi 2^3,
+result adalah hasil pengurangan dari variabel addition dikurangi 1
+"""
+addition = 2**3
+result = addition - 1
+print(result)
+
+
+"""
+Output: 7
+"""
